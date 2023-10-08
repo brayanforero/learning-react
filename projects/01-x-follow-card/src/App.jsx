62f18e80e1e57d4ca6b6a4ac020a0eaba55073bb
Brayan Forero <bforero.engineer@gmail.com>
2023-10-08 12:15:22 -0400

    add(project-1): finish project about first lesson

--- projects/01-x-follow-card/src/App.jsx
+++ projects/01-x-follow-card/src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
-import FollowCard from "./FollowCard";
+import Employ from "./Employ";
+import users from "./user.json";
 
 function App() {
   return (
     <>
-      <FollowCard userName="midudev">Brayan Forero</FollowCard>
-      <FollowCard userName="react">Bricher Rene Forero</FollowCard>
+      {users.results.map((u) => {
+        const name = `${u.name.first} ${u.name.last}`;
+        const location = `${u.location.city}, ${u.location.country}`;
+        return (
+          <Employ
+            key={u.id}
+            name={name}
+            position={location}
+            avatar={u.picture.medium}
+          />
+        );
+      })}
     </>
   );
 }
